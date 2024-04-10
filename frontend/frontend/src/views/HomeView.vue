3a12efadbadeff0ea0d2c81a90b3df85fdecbd2c
Haidoan29 <129633565+Haidoan29@users.noreply.github.com>
2024-04-10 08:53:27 +0700

    uploadcode1

--- frontend/frontend/src/views/HomeView.vue
+++ frontend/frontend/src/views/HomeView.vue
@@ -31,18 +31,6 @@ export default {
       <button type="submit">Đăng Ký</button>
     </form>
 
-    <!-- <h2>Đăng Nhập</h2>
-    <form @submit.prevent="login">
-      <div>
-        <label for="loginUsername">Tên đăng nhập:</label>
-        <input type="text" id="loginUsername" v-model="loginForm.username" required>
-      </div>
-      <div>
-        <label for="loginPassword">Mật khẩu:</label>
-        <input type="password" id="loginPassword" v-model="loginForm.password" required>
-      </div>
-      <button type="submit">Đăng Nhập</button>
-    </form> -->
   </div>
 </template>
 <script>
@@ -79,43 +67,16 @@ export default {
         this.$router.push('/about');
       } catch (error) {
         console.error('Đăng ký không thành công:', error.response ? error.response.data : error.message);
-
-        // Hiển thị thông báo lỗi cho người dùng
       }
     },
-    // async login() {
-    //     try {
-    //         const response = await axios.post('https://localhost:7074/api/Auth/Login', {
-    //             username: this.loginForm.username,
-    //             password: this.loginForm.password
-    //         });
-
-    //         // Kiểm tra kết quả từ phản hồi
-    //         if (response.status === 200) {
-    //             console.log('Đăng nhập thành công:', response.data);
-    //             // Lưu token vào local storage để sử dụng trong các yêu cầu tiếp theo
-    //             localStorage.setItem('token', response.data.token);
-    //             // Chuyển hướng người dùng đến trang dashboard hoặc trang khác
-    //             // const router = useRoter();
-    //             // router.push('/d'); // Thay đổi '/dashboard' thành đường dẫn của trang bạn muốn chuyển hướng
-    //         } else {
-    //             console.error('Đăng nhập không thành công:', response.data);
-    //             // Hiển thị thông báo lỗi cho người dùng
-    //         }
-    //     } catch (error) {
-    //         console.error('Đã xảy ra lỗi khi đăng nhập:', error);
-    //         // Hiển thị thông báo lỗi cho người dùng
-    //     }
-    // }
 
     // login() {
-    //     var url = "https://localhost:7074/api/Auth/Login";
-    //     axios.post(url).then(response => {
-    //         console.log(response);
-
-    //     }).catch(error => {
-    //         console.error('Đã xảy ra lỗi khi tải dữ liệu:', error);
-    //     })
+    //   var url = "https://localhost:7074/api/Auth/Login";
+    //   axios.post(url).then(response => {
+    //     console.log(response);
+    //   }).catch(error => {
+    //     console.error('Đã xảy ra lỗi khi tải dữ liệu:', error);
+    //   })
     // },
 
   },
@@ -123,7 +84,7 @@ export default {
     //debugger
     console.log('Component created');
     this.titlePage = "Component Setting in created";
-    // this.loadData();
+    //this.loadData();
   }
 }
 
