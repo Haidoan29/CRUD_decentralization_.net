dc1e8d1895f35783d0e9f4c9f87ceac501442843
Haidoan29 <129633565+Haidoan29@users.noreply.github.com>
2024-04-20 09:38:33 +0700

    upload

--- frontend/frontend/src/views/ResetPassword.vue
+++ frontend/frontend/src/views/ResetPassword.vue
@@ -1,25 +1,19 @@
 <template>
-    <div>
-        <Heders />
-    </div>
     <div class="home">
         <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-        <Login />
+        <ResetPassword />
         <!--msg="Welcome to Your Vue.js App"-->
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Login from '@/components/Login.vue';
-import Heders from '@/components/heder.vue'
+import ResetPassword from '@/components/ResetPassword.vue';
 
 export default {
     name: 'HomeView',
     components: {
-        Login,
-        Heders
-
+        ResetPassword
     }
 }
 </script>
